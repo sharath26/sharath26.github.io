cfc1799008261a07eaa0bc18617e6badb9ae76e9
sharath26 <sharathmm51@gmail.com>
2020-09-21 10:23:09 +0530

    My first Commit

---
+++ GMS/js/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    var table = $('#example').DataTable({
+        responsive: true
+    });
+
+    new $.fn.dataTable.FixedHeader(table);
+});
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
